99049383b09d079a6b1db1118c6d6143a984ce86
haizhou85 <60331375+haizhou85@users.noreply.github.com>
2020-03-12 16:07:00 -0700

    Game set up

--- my-react-app/src/Components/box.js
+++ my-react-app/src/Components/box.js
@@ -4,36 +4,98 @@ import '../App.css';
 class Box extends Component {
 constructor(){
         super()
+        this.handelClick = this.handelClick.bind(this);
         this.state = {
             picture: "",
             boxClicked: 0,
-            canClick: this.boxCaller.bind(this)
+            canClick: this.boxCaller.bind(this),
+            name: "box",
         }
     }
 
 
     changePicture(value){
-        if (value === 0){
-            this.setState({ picture: "❌"})
-        } else if (value){
-            this.setState({ picture: "💥"})
+        this.setState({ picture: "0"})
+            if (value === 9){
+            this.setState({ picture: "💣"})
+        } else if (value === 0) {
+            this.numberStyle(value)
+            this.setState({ picture: ""})
+        }else {
+            this.numberStyle(value)
+            this.setState({ picture: value})
         }
     }
 
+    numberStyle(value){
+        if (value === 0){
+            this.setState({ name: 'box box0'})
+        }
+        else if (value === 1){
+            this.setState({ name: 'box box1'})
+        }
+        else if (value === 2){
+            this.setState({ name: 'box box2'})
+        }
+        else if (value === 3){
+            this.setState({ name: 'box box3'})
+        }
+        else if (value === 4){
+            this.setState({ name: 'box box4'})
+        }
+        else if (value === 5){
+            this.setState({ name: 'box box5'})
+        }
+        else if (value === 6){
+            this.setState({ name: 'box box6'})
+        }
+        else if (value === 7){
+            this.setState({ name: 'box box7'})
+        }
+        else if (value === 8){
+            this.setState({ name: 'box box8'})
+        }
+    }
 
-    boxCaller() {
-        if (this.props.canShoot === 0 ){
-            if (this.state.boxClicked === 0){
-                this.props.clickFunction(this.props.value, this.props.ammo);
+    handelClick = (e) => {
+        if (e.altKey) {
+            if (this.state.picture !== "🚩" && this.state.picture !== this.props.value){
+                this.setState({ picture: "🚩"})
                 this.setState({ boxClicked: 1})
             }
+            else if (this.state.picture === "🚩"){
+                this.setState({ picture: ""})
+                this.setState({ boxClicked: 0})
+
+            }
+
+        } else {
+         this.boxCaller()
+        }
+    }
+
+    cascade(index1, index2) {
+
+    }
+
+    boxCaller() {
+        if (this.state.boxClicked === 0 && this.state.picture !== "🚩"){
             this.changePicture(this.props.value)
+            this.setState({ boxClicked: 1})
+            if (this.props.value === 0) {
+                this.cascade(this.props.index1, this.props.index2)
+            }
+        }
+        else {
+
         }
     }
     render(){
         return (
-            <div className = "box" onClick = { this.state.canClick }>
+            <div className = {this.state.name} onClickCapture = { this.handelClick } ref = {this.canClick}>
+
                 {this.state.picture}
+
             </div>
         );
     }
--- my-react-app/src/Components/game.js
+++ my-react-app/src/Components/game.js
@@ -4,14 +4,63 @@ import Box from './box';
 class Game extends Component {
     constructor(props) {
         super(props)
+        this.bombs = 30
         this.state = {
             board: this.setUp()
         }
     }
 
     setUp = () => {
-        let tempArray = Array(10).fill().map(n => Array(10).fill(0))
-        return tempArray
+        let gameBoard = Array(10).fill().map(n => Array(10).fill(0))
+        console.log(gameBoard);
+        gameBoard = this.bombPlace(gameBoard)
+        return gameBoard
+    }
+
+    bombPlace = (gameBoard) => {
+        for (let i = 0; i < this.bombs; i++) {
+            let vertical = Math.floor(Math.random()*gameBoard.length)
+            let horizontal = Math.floor(Math.random()*gameBoard.length)
+            if (gameBoard[vertical][horizontal] < 9 ) {
+                gameBoard[vertical][horizontal] = 9
+                if (typeof(gameBoard[vertical][horizontal+1]) === "number"){
+                    gameBoard[vertical][horizontal+1] += 1
+                }
+                if (typeof(gameBoard[vertical][horizontal-1]) === "number"){
+                    gameBoard[vertical][horizontal-1] += 1
+                }
+                //-------------------------------------------
+                if (typeof(gameBoard[vertical+1]) !== "undefined"){
+                    if (typeof(gameBoard[vertical+1][horizontal]) === "number" ){
+                    gameBoard[vertical+1][horizontal] += 1
+                    }
+                    if (typeof(gameBoard[vertical+1][horizontal+1]) === "number"){
+                        gameBoard[vertical+1][horizontal+1] += 1
+                    }
+                    if (typeof(gameBoard[vertical+1][horizontal-1]) === "number"){
+                        gameBoard[vertical+1][horizontal-1] += 1
+                }}
+                if (typeof(gameBoard[vertical-1]) !== "undefined"){
+                    if (typeof(gameBoard[vertical-1][horizontal-1]) === "number"){
+                        gameBoard[vertical-1][horizontal-1] += 1
+                    }
+                    if (typeof(gameBoard[vertical-1][horizontal+1]) === "number"){
+                        gameBoard[vertical-1][horizontal+1] += 1
+                    }
+                    if (typeof(gameBoard[vertical-1][horizontal]) === "number"){
+                        gameBoard[vertical-1][horizontal] += 1
+                }}
+            } else {
+                i--
+            }
+        } for (let i = 0; i < gameBoard.length; i++) {
+            for (let j = 0; j < gameBoard.length; j++) {
+                if (gameBoard[i][j] > 8){
+                    gameBoard[i][j] = 9
+                }
+            }
+        }
+        return gameBoard
     }
 
 
@@ -22,7 +71,9 @@ class Game extends Component {
         finish.push(board[i].map((value,index) => {return(
             <Box
                 value = {value}
-                index = {index}
+                index2 = {index}
+                index1 = {i}
+                gameBoard = {board}
             />)}
         ));
     }
